e2781f809dcbb21e5bfd5c9bc76af1fde18ca5fa
saadetcatakakgunes1211 <saadetcatak1453@gmail.com>
2023-10-03 22:54:02 +0300

    Update

--- MyAcademyCarBook.DataAccessLayer/Concrete/CarBookContext.cs
+++ MyAcademyCarBook.DataAccessLayer/Concrete/CarBookContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using MyAcademyCarBook.EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,20 @@ using System.Threading.Tasks;
 
 namespace MyAcademyCarBook.DataAccessLayer.Concrete
 {
-   public class CarBookContext
+   public class CarBookContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=SAADET\\SQLEXPRESS01; initial catalog=DbCarBook;integrated security=true");
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarCategory> CarsCategories { get; set; }
+        public DbSet<CarStatus> CarsStatuses { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Price> Prices { get; set; }
+
     }
     
 }

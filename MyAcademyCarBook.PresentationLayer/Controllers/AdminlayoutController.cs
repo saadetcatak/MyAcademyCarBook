fe628c8473557531beda49ca2df67201c6d91f82
saadetcatakakgunes1211 <saadetcatak1453@gmail.com>
2023-10-12 22:07:52 +0300

    update

---
+++ MyAcademyCarBook.PresentationLayer/Controllers/AdminlayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAcademyCarBook.PresentationLayer.Controllers
+{
+    public class AdminlayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

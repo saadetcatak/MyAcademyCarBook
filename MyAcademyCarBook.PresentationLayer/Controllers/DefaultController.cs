9067cd6aa0f33c001f16152204de9afc06f4c4d5
saadetcatakakgunes1211 <saadetcatak1453@gmail.com>
2023-11-23 11:18:22 +0300

    update

--- MyAcademyCarBook.PresentationLayer/Controllers/DefaultController.cs
+++ MyAcademyCarBook.PresentationLayer/Controllers/DefaultController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MyAcademyCarBook.BusinessLayer.Abstract;
+using MyAcademyCarBook.EntityLayer.Concrete;
+using System.Text.Json;
 
 namespace MyAcademyCarBook.PresentationLayer.Controllers
 {
@@ -14,18 +17,127 @@ namespace MyAcademyCarBook.PresentationLayer.Controllers
 
         public IActionResult Index()
         {
+            var cars = _carService.TGetListAll();
+
+            IEnumerable<SelectListItem> models = (from x in cars
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CarName,
+                                                      Value = x.CarName,
+
+                                                  }).ToList();
+
+            ViewBag.cars = models;
+            var gasList = cars.Select(x => x.GasType).Distinct().ToList();
+            IEnumerable<SelectListItem> gasTypes = (from x in gasList
+                                                    select new SelectListItem
+                                                    {
+                                                        Text = x,
+                                                        Value = x,
+
+                                                    }).ToList();
+
+            ViewBag.gas = gasTypes;
+
+            var geatList = cars.Select(x => x.GeatType).Distinct().ToList();
+            IEnumerable<SelectListItem> geatTypes = (from x in geatList
+                                                     select new SelectListItem
+                                                     {
+                                                         Text = x,
+                                                         Value = x,
+
+                                                     }).ToList();
+
+            ViewBag.geat = geatTypes;
             return View();
         }
 
-
-        public IActionResult CarListFilter(string Model,string GeatType,int Year,string BrandName)
+        [HttpGet]
+        public PartialViewResult FilterCars()
         {
-            var cars = _carService.TGetCarByFilters(Model, GeatType, Year, BrandName);
-            ViewBag.Model = Model;
-            ViewBag.GeatType= GeatType;
-            ViewBag.BrandName= BrandName;
-            ViewBag.Year = Year;
-            return View(cars);
+            var cars = _carService.TGetListAll();
+
+            IEnumerable<SelectListItem> models = (from x in cars
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CarName,
+                                                      Value = x.CarName,
+
+                                                  }).ToList();
+
+            ViewBag.cars = models;
+
+            var gasList = cars.Select(x => x.GasType).Distinct().ToList();
+            IEnumerable<SelectListItem> gasTypes = (from x in gasList
+                                                    select new SelectListItem
+                                                    {
+                                                        Text = x,
+                                                        Value = x,
+
+                                                    }).ToList();
+
+            ViewBag.gas = gasTypes;
+
+            var geatList = cars.Select(x => x.GeatType).Distinct().ToList();
+            IEnumerable<SelectListItem> geatTypes = (from x in geatList
+                                                     select new SelectListItem
+                                                     {
+                                                         Text = x,
+                                                         Value = x,
+
+                                                     }).ToList();
+
+            ViewBag.geat = geatTypes;
+            return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult FilterCars(Car car)
+        {
+            ViewData["model"] = car.CarName;
+            ViewData["year"] = car.Year;
+            ViewData["gasType"] = car.GasType;
+            ViewData["geatType"] = car.GeatType;
+
+
+            var values = _carService.TGetAllCarsWithBrands();
+
+
+
+
+            if (!string.IsNullOrEmpty(car.CarName) || car.Year != null || !string.IsNullOrEmpty(car.GasType) || !string.IsNullOrEmpty(car.GeatType))
+            {
+
+
+                var lowerCaseModel = car.CarName.ToLower();
+                var lowerCaseGasType = car.GasType.ToLower();
+                var lowerCaseGearType = car.GeatType.ToLower();
+                values = values.Where(x => x.CarName.ToLower().Contains(lowerCaseModel) && x.Year >= car.Year && x.GasType.ToLower() == lowerCaseGasType && x.GeatType.ToLower() == lowerCaseGearType).ToList();
+
+
+                TempData["filteredCars"] = JsonSerializer.Serialize(values);
+                return RedirectToAction("Index", "RentCar");
+
+
+
+
+
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
+
+
+
+        //public IActionResult CarListFilter(string Model,string GeatType,int Year,string BrandName)
+        //{
+        //    var cars = _carService.TGetCarByFilters(Model, GeatType, Year, BrandName);
+        //    ViewBag.Model = Model;
+        //    ViewBag.GeatType= GeatType;
+        //    ViewBag.BrandName= BrandName;
+        //    ViewBag.Year = Year;
+        //    return View(cars);
+        //}
+    
